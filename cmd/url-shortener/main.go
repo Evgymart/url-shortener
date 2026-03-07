9e982c0b0ddf2b07f8482be477fe874c69bb498e
Evgymart <alignant@yandex.ru>
2026-03-07 13:51:50 +0300

    Init logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,10 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"urlshort/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
+	logger := setupLogger(cfg.Env)
 	fmt.Println(cfg)
+	logger.Info("starting server", slog.String("env", cfg.Env))
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
